e5ccecd94146f205cd3d9ee5386c2c0260d4acde
imtinge <imtinge@163.com>
2017-07-16 23:25:47 +0800

    modify carrierwave.rb for env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,10 @@
-::CarrierWave.configure do |config|
+CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = "your qiniu access_key"
-  config.qiniu_secret_key    = 'your qiniu secret_key'
-  config.qiniu_bucket        = "carrierwave-qiniu-example"
-  config.qiniu_bucket_domain = "carrierwave-qiniu-example.aspxboy.com"
-  config.qiniu_bucket_private= true #default is false
+  config.qiniu_access_key    = ENV["qiniu_access_key"]
+  config.qiniu_secret_key    = ENV["qiniu_secret_key"]
+  config.qiniu_bucket        = ENV["qiniu_bucket"]
+  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
-
-  config.qiniu_up_host       = 'http://up.qiniug.com' #七牛上传海外服务器,国内使用可以不要这行配置
+  config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，up.qiniug.com 不同
 end
